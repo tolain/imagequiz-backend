0d0f70ddad552aeba388af60272a2904985a0e62
Johntolu <76501859+Johntolu@users.noreply.github.com>
2021-11-20 11:59:11 -0800

    .

--- api.js
+++ api.js
@@ -1,7 +1,22 @@
-
+var { customers } = require('./data_tier/customers');
 
 let add = (n, m) => {
     return n + m;
 }
 
+let getCustomers = () => {
+    return customers;
+}
+
+let addCustomer = (name, email, password) => {
+    let alreadyExists = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
+    if(alreadyExists) {
+        return true;
+    }
+    customers.push({id: customers.length + 1, name: name, email: email, password: password});
+    return false;
+}
+
 exports.add = add;
+exports.addCustomer = addCustomer;
+exports.getCustomers = getCustomers;
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const cors = require('cors');
 const api = require('./api');
 
 
 
 const application = express();
-const port = process.env.PORT || 4002;
+const port = process.env.PORT || 4003;
+application.use(cors());
+application.use(express.json());
 
 application.get('/add/:n/:m', (request, response) => {
     let n = Number(request.params.n);
     let m = Number(request.params.m);
     let sum = api.add(m, n);
-    response.send('${n} +  ${m} = ${sum}.');
+    response.send(`${n} +  ${m} = ${sum}.`);
 
+});
+
+application.get('/customers', (request, response) => {
+    response.json(api.getCustomers());
+
+application.post('/register', (request, response) => {
+    let name = request.body.name;
+    let email = request.params.email;
+    let password = request.body.password;
+    let alreadyExists = api.addCustomer(name, email, password);
+    if(alreadyExists) {
+        response.status(403).json({message: 'A customer with the same email already exists.'})
+    } else {
+        response.json({message: 'The customer added.'});
+    }
 });
 
 
