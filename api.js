2e62829081f0c244becb1d2733c0725665a67532
Johntolu <76501859+Johntolu@users.noreply.github.com>
2021-11-21 11:57:33 -0800

    .

--- api.js
+++ api.js
@@ -1,4 +1,7 @@
 var { customers } = require('./data_tier/customers');
+var { flowers } = require('./data_tier/flowers');
+var { quizzes } = require('./data_tier/data');
+var { scores } = require('./data_tier/scores');
 
 let add = (n, m) => {
     return n + m;
@@ -6,6 +9,19 @@ let add = (n, m) => {
 
 let getCustomers = () => {
     return customers;
+}
+
+let sub = (n, m) => {
+    return n - m;
+}
+
+let getFlowers = () => {
+    let flowerName=[];
+    for(let i = 0; i < flowers.length; i++){
+        flowerName.push(flowers[i].name);
+    }
+    return flowerName;
+
 }
 
 let addCustomer = (name, email, password) => {
@@ -17,6 +33,47 @@ let addCustomer = (name, email, password) => {
     return false;
 }
 
+let getQuizID = (id) => {
+    return quizzes[id];
+}
+
+let addCustomer = (name, email, password) => {
+    let alreadyExist = customers.find(x=> x.email.toLowerCase() === email.toLowerCase());
+    if(alreadyExist){
+        return true;
+    }
+    customers.push({id: customers.length + 1, name: name, email: email, password: password});
+    return false;
+}
+
+let customerLogin = ( email, password) => {
+    let isValid = customers.find(x => x.email.toLowerCase() === email.toLowerCase() && x.password == password);
+    if(isValid){
+        return true;
+    }
+    return false;
+}
+
+let addScore = (quizTaker, quizID, score) => {
+    scores.push({quizTaker, quizID, score});
+}
+
+let checkUserScore = (quizTaker, quizID) => {
+    for(let i = 0; i < scores.length; i++ ){
+        if(scores[i] === quizTaker && scores[i].quizID === quizID) {
+            return scores[i].score;
+        }
+    }
+    return -1;
+}
+
+
 exports.add = add;
-exports.addCustomer = addCustomer;
+exports.getFlowers = getFlowers;
 exports.getCustomers = getCustomers;
+exports.sub = sub;
+exports.addCustomer = addCustomer;
+exports.getQuizID = getQuizID;
+exports.customerLogin = customerLogin;
+exports.addScore = addScore;
+exports.checkUserScore = checkUserScore;
